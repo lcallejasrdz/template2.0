ccd8fa194e80c46e32428a0e349ebd5d2f8f5bf6
vagrant <vagrant@homestead>
2019-06-09 04:43:52 +0000

    Login Finished

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Http\Request\LoginRequest;
 
 use Sentinel;
 use Redirect;
@@ -14,7 +15,7 @@ class AuthController extends Controller
         return view('auth.login');
     }
 
-    public function postLogin(Request $request)
+    public function postLogin(LoginRequest $request)
     {
         $credentials = [
             'email'    => $request->email,
