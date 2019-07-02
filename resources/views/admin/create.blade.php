4767d6be96f0332bb274b56bcdb3cb833d8fdc22
vagrant <vagrant@homestead>
2019-07-02 04:04:47 +0000

    is invalid in inputs

--- resources/views/admin/create.blade.php
+++ resources/views/admin/create.blade.php
@@ -16,7 +16,7 @@
 		<div class="col-sm-12">
 			{!! Form::open(['route' => $active.'.store', 'method' => 'post', 'id' => 'formValidation', 'class' => 'form-horizontal', 'files' => true]) !!}
 				<div id="rootwizard">
-				    <div class="form-group {{ $errors->first('name') ? 'has-error' : '' }}">
+				    <div class="form-group {{ $errors->first('name') ? 'is-invalid' : '' }}">
 				        {!! Form::label('name', trans('validation.attributes.name').' *', ['class' => 'col-sm-2 control-label']) !!}
 				        <div class="col-sm-12">
 				            {!! Form::text('name', old('name'), ['id' => 'name', 'class' => 'form-control', 'placeholder' => trans('validation.attributes.name')]) !!}
@@ -24,7 +24,7 @@
 				        </div>
 				    </div>
 
-				    <div class="form-group {{ $errors->first('description') ? 'has-error' : '' }}">
+				    <div class="form-group {{ $errors->first('description') ? 'is-invalid' : '' }}">
 				        {!! Form::label('description', trans('validation.attributes.description').' *', ['class' => 'col-sm-2 control-label']) !!}
 				        <div class="col-sm-12">
 				            {!! Form::textarea('description', old('description'), ['id' => 'description', 'class' => 'form-control', 'placeholder' => trans('validation.attributes.description')]) !!}
@@ -32,7 +32,7 @@
 				        </div>
 				    </div>
 
-				    <div class="form-group {{ $errors->first('price') ? 'has-error' : '' }}">
+				    <div class="form-group {{ $errors->first('price') ? 'is-invalid' : '' }}">
 				        {!! Form::label('price', trans('validation.attributes.price').' *', ['class' => 'col-sm-2 control-label']) !!}
 				        <div class="col-sm-12">
 				            {!! Form::text('price', old('price'), ['id' => 'price', 'class' => 'form-control', 'placeholder' => trans('validation.attributes.price')]) !!}
