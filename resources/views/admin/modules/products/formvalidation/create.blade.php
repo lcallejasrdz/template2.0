bf40e972137e30f2b24d0cc2729d96d45fe28124
vagrant <vagrant@homestead>
2019-07-02 03:57:58 +0000

    Create Product Form

--- resources/views/admin/modules/products/formvalidation/create.blade.php
+++ resources/views/admin/modules/products/formvalidation/create.blade.php
@@ -37,11 +37,6 @@
 	                bootstrap: new FormValidation.plugins.Bootstrap(),
 	                submitButton: new FormValidation.plugins.SubmitButton(),
 	                defaultSubmit: new FormValidation.plugins.DefaultSubmit(),
-	                icon: new FormValidation.plugins.Icon({
-	                    valid: 'fa fa-check',
-	                    invalid: 'fa fa-times',
-	                    validating: 'fa fa-refresh',
-	                }),
 	            },
 	        }
 	    );
