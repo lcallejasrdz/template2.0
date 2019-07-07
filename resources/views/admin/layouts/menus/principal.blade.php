b776010d4d9a65ef5945f1df40dea559e6a9c8e6
vagrant <vagrant@homestead>
2019-07-07 18:32:34 +0000

    Cambio Menu Productos

--- resources/views/admin/layouts/menus/principal.blade.php
+++ resources/views/admin/layouts/menus/principal.blade.php
@@ -36,9 +36,9 @@
         <div id="collapseProduct" class="collapse" aria-labelledby="headingProduct" data-parent="#accordionSidebar">
             <div class="bg-white py-2 collapse-inner rounded">
                 <h6 class="collapse-header">Acciones:</h6>
-                <a class="collapse-item" href="{!! URL::route('products.create') !!}">Crear</a>
-                <a class="collapse-item" href="cards.html">Editar</a>
-                <a class="collapse-item" href="cards.html">Eliminar</a>
+                <a class="collapse-item" href="{!! URL::route('products') !!}">Lista</a>
+                <a class="collapse-item" href="{!! URL::route('products.create') !!}">Nuevo</a>
+                <a class="collapse-item" href="#">Eliminados</a>
             </div>
         </div>
     </li>
